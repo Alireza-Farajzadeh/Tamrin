b4ff92bab9534813149fe28fc16a01ef792fa7ef
Alireza-Farajzadeh <farajzade.rezaali@gmail.com>
2024-10-27 11:10:08 +0330

    test2

--- main.py
+++ main.py
@@ -11,3 +11,27 @@ class CustomStr:
             split_string = a
 
         return split_string
+
+    def remove_duplicate(self):
+        char_see = set()
+        result = []
+
+        for i in self.string:
+            if i not in char_see:
+                char_see.add(i)
+                result.append(i)
+
+        return "".join(result)
+
+    def is_float(self):
+        a = "1.2"
+        try:
+            float(a)
+            return True
+        except:
+            return False
+
+    def is_palindrome(self):
+        if self.string == self.string.reversed():
+            return True
+        return False
