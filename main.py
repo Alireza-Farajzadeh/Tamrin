7300735aac5b6e4fe842ac24c9ec251e157e3851
Alireza-Farajzadeh <farajzade.rezaali@gmail.com>
2024-10-27 10:03:30 +0330

    .

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ class CustomStr:
     def __init__(self, string):
         self.string = string
 
-    def custom_split(self,*splits):
-        pass
+    def custom_split(self, *splits):
+        split_string = [self.string]
+        for i in splits:
+            a = []
+            for q in split_string:
+                a.extend(q.split(i))
+            split_string = a
+
+        return split_string
